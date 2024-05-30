efccd19b23dcdbf300ffe2024afbbb78b3f7fccd
Ozyugoo <stellaozy22@gmail.com>
2024-05-30 17:30:48 +0100

    Array challenge and Object intro

---
+++ assignmentDataStructure/objectsIntro.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Introduction to Objects﻿
+// Create an object called myCountry for a country of your choice, containing properties country, capital, language, population and neighbours (an array like we used in previous assignments).
+
+const myCountry = {
+  country: 'Nigeria',
+  capital: 'Abuja',
+  Language: ['Igbo', 'Yoruba', 'Hausa', 'English'],
+  population: 218,
+  neighbours: ['Chad', 'Niger', 'Cameroun', 'Benin']
+}
+
+// Dot && Bracket Notation
+
